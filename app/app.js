c0ecc9b44c4540580414d63dae53951bb47bf69b
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-12-12 16:47:22 -0600

    leaving halfway thru getting page setup to make a new branch

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-poot
+var app = angular.module("footballGurus", 
+	['firebase', 'ngRoute']);
+
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider
+			.when('/splash' {
+				templateUrl: './app/partials/splash.html',
+				controller: 'loginCtrl'
+			});
+	}]);
---
+++ app/controllers/loginCtrl.js
@@ -0,0 +1,6 @@
+app.controller("loginCtrl", ["$scope", "$q", "$http", "$firebaseArray", 
+	function($scope, $q, $http, $firebaseArray) {
+
+
+		
+	}]);
