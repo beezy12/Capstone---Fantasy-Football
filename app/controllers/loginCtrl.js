cdaca88f76530f30d25ddf10ffb760f893185a69
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-12-13 20:15:48 -0600

    adding user and saving first and last names to firebase

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -2,11 +2,38 @@ app.controller("loginCtrl", ["$scope", "$q", "$http", "$firebaseArray",
 	function($scope, $q, $http, $firebaseArray) {
 
 
-		$scope.registerMe = function() {
-			console.log("hear ya register button");
+		$scope.loginEmail = "";
+		$scope.loginPassword = "";
+		$scope.firstName = "";
+		$scope.lastName = "";
+
+		var ref = new Firebase("https://capstonefootball.firebaseio.com/user");
+		console.log("ref is ", ref);
+
+		$scope.registerUser = function() {
+			ref.createUser({
+				email: $scope.loginEmail,
+				password: $scope.loginPassword
+			}, function(error, userData) {
+				if (error) {
+					console.log("you messed up something");
+				} else {
+					console.log("you made a profile with user Id of: ", userData.uid);
+					currentUid = userData.uid;
+					console.log("currentUid", currentUid);
+
+					ref.child("/"+userData.uid).set({
+						"First Name": $scope.firstName,
+						"Last Name": $scope.lastName
+					});
+				}
+			});
 		};
 
-		$scope.signMeIn = function() {
+		
+
+
+		$scope.signUserIn = function() {
 			console.log("hear that ol sign in button being clicked");
 		};
 	}]);
