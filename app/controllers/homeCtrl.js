d60f2d6aab3a24a28e6e61d12ea7c510d2ea147c
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-12-16 11:31:54 -0600

    got onauth working

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -1,12 +1,13 @@
-app.controller("homeCtrl", ["$scope", "$q", "$http", "$firebaseArray", "$location", "$rootScope",
-	function($scope, $q, $http, $firebaseArray, $location, $rootScope) {
+app.controller("homeCtrl", ["$scope", "$q", "$http", "$firebaseArray", "$location", "$rootScope", "generalVariables",
+	function($scope, $q, $http, $firebaseArray, $location, $rootScope, generalVariables) {
 		
 		var userRef = new Firebase("https://capstonefootball.firebaseio.com/user");
 
 		var userArray = $firebaseArray(userRef);
 		console.log("userArray", userArray);
 
-
+		generalVariables.checkUserLogin();
+		console.log("method checkUserLogin fired....not sure if it's in the right spot");
 
 
 }]);
