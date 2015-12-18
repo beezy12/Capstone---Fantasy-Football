6d77a222a690c3a182b58a181316d86072e03d67
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-12-17 19:00:25 -0600

    deleting from draft list and outputting to dom...but duplicates

--- app/controllers/draftCtrl.js
+++ app/controllers/draftCtrl.js
@@ -1,10 +1,11 @@
-app.controller('draftCtrl', ["$scope", "$q", "$http", "$firebaseArray", "generalVariables",
-	function($scope, $q, $http, $firebaseArray, generalVariables) {
+app.controller('draftCtrl', ["$scope", "$q", "$http", "$firebaseArray", "$firebaseObject", "generalVariables",
+	function($scope, $q, $http, $firebaseArray, $firebaseObject, generalVariables) {
 
 		// generalVariables.setHeight();
 
+		window._ = _;
 		
-		generalVariables.getUid();
+		var currentUid = generalVariables.getUid();
 		// generalVariables.checkUserLogin();
 
 
@@ -36,20 +37,24 @@ app.controller('draftCtrl', ["$scope", "$q", "$http", "$firebaseArray", "general
 
 		var playerRef = new Firebase ("https://capstonefootball.firebaseio.com/zplayersList");
 
+		var teamPlayersRef = new Firebase("https://capstonefootball.firebaseio.com/teamPlayers");
+
 		// empty array that will hold players AngularFire array that comes back when promise is complete
 		$scope.loadedPlayers = [];
+
+		$scope.teamPlayersArray = [];
 
 		// takes the Firebase ref for the zPlayersList and turns it from an object into an array, which is
 		// then processed through a promise and returned to the empty array above
 		var playerArray = $firebaseArray(playerRef);
-		console.log("playerArray-->", playerArray);
+		//console.log("playerArray-->", playerArray);
 
 
 		playerArray
 			.$loaded()
 			.then(function(data) {
 				$scope.loadedPlayers = playerArray;
-				console.log("$scope.loadedPlayers--->", $scope.loadedPlayers);
+				//console.log("$scope.loadedPlayers--->", $scope.loadedPlayers);
 			});
 
 		//set player id
@@ -58,41 +63,168 @@ app.controller('draftCtrl', ["$scope", "$q", "$http", "$firebaseArray", "general
 			for (var i=0; i < players.length; i++){
 				if (yo === players[i].$id) {
 					$scope.modalPlayer = players[i];
-					console.log("players[i]", players[i]);
+					// console.log("players[i]", players[i]);
 				}
 			}
 		};
 
-			
+		
 
-		var teamPlayersRef = new Firebase("https://capstonefootball.firebaseio.com/teamPlayers");
+		// PUT PROMISE ON LOADEDPLAYERS AND THEN DO THIS BELOW
+
+		var arr = [];
+		var thang =[]
+		$scope.benPlayers = [];
+		var draftlist = [];
+		// watches for changes to firebase teamPlayers object (which is now an array)
+		var teamPlayersArray = $firebaseArray(teamPlayersRef);
+		// angular.element(".draftMyTeam").html("");
+		teamPlayersArray.$watch(function(snapshot) { 
+
+			teamPlayersArray.$loaded()
+			.then(function(draftedPlayers){
+				console.log("draftedPlayers ", draftedPlayers);
+
+				$firebaseArray(playerRef).$loaded()
+				.then(function(allPlayers){
+					console.log("allPlayers ", allPlayers);
+
+					for(var s = 0; s < draftedPlayers.length; s++){
+						_.filter(allPlayers, function(index){
+							// console.log("index ", index);
+							if(draftedPlayers[s].$id ===  index.$id){
+								thang.push(index);
+								$scope.benPlayers.push(index);
+							}
+						});
+					}
+
+					console.log("thang ", thang);
+				})
+
+			});
+		});
+		// teamPlayersArray.$watch(function(snapshot) { 
+
+		// 	console.log("teamPlayersArray ", teamPlayersArray);
+
+		// 	for(var i =0; i < teamPlayersArray.length; i++){
+		// 		console.log("current item is ", teamPlayersArray[i]);
+
+		// 		//if value of player id in teamPlayers assosiated with current users uid, push that playerId into arr
+		// 		if(teamPlayersArray[i].$value === generalVariables.getUid()){
+		// 			console.log("there is a match");
+		// 			arr.push(teamPlayersArray[i].$id);
+
+		// 		}
+		// 	}
+
+		// 	//call callback function here to house code below
+
+
+		// 	console.log("arr ", arr);
+
+		// 	$firebaseArray(playerRef).$loaded()
+		// 	.then(function(players){
+
+		// 		console.log("players ", players);
+
+		// 			//loop over all players who are associated with the current user's id
+		// 			for(var x = 0; x < arr.length; x++){
+
+		// 				for(var z = 0; z < players.length; z++){
+
+		// 					//if there is a match
+		// 					if(arr[x] === players[z].$id){
+		// 						$scope.benPlayers.push(players[z]);
+		// 					}
+
+		// 						//push that player object into our output array
+		// 				}
+
+		// 			}
+
+		// 	})
+		// });
+
+
+
+			// $scope.loadedPlayers
+			// 	.$loaded()
+			// 	.then(function(stuff) {
+
+			// 		// $scope.wut = $scope.loadedPlayers;
+			// 		console.log("hey snapshot ", snapshot)
+
+			// 		// arr.push(snapshot.key)
+			// 		// console.log(snapshot.val());
+		 //  			// console.log("teamPlayers changed! iterate over to update team roster");
+		 //  			console.log($scope.loadedPlayers)
+		 //  			for (var i = 0; i < arr.length; i++) {
+		 //  				for (var j = 0; j < $scope.loadedPlayers.length; j++) {
+
+		 //  					if (arr[i] === $scope.loadedPlayers[j].$id) {
+		 //  						// console.log("first ----->", arr[i])
+		 //  						// console.log("second ----->", teamPlayersArray[j].$id)
+		 //  						var tA = $scope.loadedPlayers[j];
+		 //  						draftlist.push({ displayName: tA['displayName'], team: tA['team'] })
+		 //  						console.log("yo")
+		  						
+		 //  					};	
+		 //  				}
+		 //  			}
+
+		 //  	});
+
+		// });
+
+		
+
+		$scope.allDemPlayers = $firebaseObject(playerRef);
+		
+
+		// teamPlayersRef.on('value', function(snapshot) {
+		// 	var obj = snapshot.val();
+		// 	console.log("snapshot object --->", obj);
+
+		// 	for (var key in obj) {
+		// 		if (obj[key] === currentUid) {
+		// 			// console.log("obj[key]", obj[key]);
+		// 			// console.log("key", key);
+		// 			// console.log("true");
+		// 			console.log('current player', $scope.allDemPlayers[key]);
+		// 			$scope.myPlayers.push($scope.allDemPlayers[key]);
+
+
+		// 			// got user's key here, which is the value of the player key in teamPlayers obj, so need to get a ref
+		// 			// to player's info for output to each team
+		// 		}
+		// 	}
+
+		// 	console.log("scope.myPlayers", $scope.myPlayers);
+		// });
+
+		
 		
 		//push playerid:userId to teamPlayers in firebase
 		$scope.draftPlayer = function() {
 			teamPlayersRef.child($scope.modalPlayer.$id).set(generalVariables.getUid());
 			console.log("teamPlayersRef.child($scope.modalPlayer.$id) -----> here", teamPlayersRef);
 
-			//get ref to players, set drafted to true
+			//get ref to player's 'drafted' key, set 'drafted' to true
 			playerRef.child($scope.modalPlayer.$id).child("drafted").set(true);
-
-			// if drafted === true, add player to currentUid team list on draft list AND team homepage, and hide player card from draft list
-			// if ($scope.modalPlayer.$id).child("drafted" === true) {
-			// 	when $scope.myValue is truthy 
-			// }
-			
-			// deletes player from draft list after pick
-			// $scope.loadedPlayers.child.$remove($scope.modalPlayer.$id);
-
-			
 		};
 
 
 				
+}]);
 
 
 
 
 			
 
-}]);
 
+
+
+			
